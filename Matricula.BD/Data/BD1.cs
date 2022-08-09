ef0aeed087fb3133ad336b1af026fd89ca8a8101
Hoffman Erwin <erwinhoffman03@gmail.com>
2022-08-09 02:06:25 -0300

    Agregar archivos de proyecto.

---
+++ Matricula.BD/Data/BD1.cs
@@ -0,0 +1,19 @@
+﻿using Matricula.BD.Data.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matricula.BD.Data
+{
+    public class BD1 : DbContext
+    {
+
+        public DbSet<Medico> Medicos { get; set; }
+        public BD1(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ Matricula.BD/Migrations/20220809050306_inicio.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Matricula.BD.Migrations
+{
+    public partial class inicio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medicos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DNI = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medicos");
+        }
+    }
+}
