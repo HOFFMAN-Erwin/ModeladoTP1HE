96aab68c195bae14d24a8a49af7850669bc0f888
Hoffman Erwin <erwinhoffman03@gmail.com>
2022-08-21 23:57:57 -0300

    codigo UQ

--- Matricula.BD/Data/Entidades/Especialidad.cs
+++ Matricula.BD/Data/Entidades/Especialidad.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace Matricula.BD.Data.Entidades
 {
+    [Index(nameof(Codigo),Name="ProveedorCodigo_UQ",IsUnique =true)]
     public class Especialidad:EntityBase
     {
         [Required]
